ec3b856bb2b135ca4a8e98263439e03899d758e1
Nils Schneider-Sturm <nils.schneider-sturm@htl.rennweg.at>
2020-10-06 15:02:38 +0200

    added preview function

--- php/video.php
+++ php/video.php
@@ -3,4 +3,5 @@
 interface VideoInterface {
     function getHTMLEmbedded(); // zb <iframe..... oder <video....
     function getName(); // entweder youtube, vimeo oder local
+    function getPreviewUrl(); // url von bild aus video
 }
